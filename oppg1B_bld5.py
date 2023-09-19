02fcfa8811a2e2f92e80a1ea9ebaa52c2dbf7da6
HussamBasim <basimhussam1@gmail.com>
2023-09-19 12:41:29 +0200

    Celcius til farenheit lagdt inn
    hei

--- oppg1B_bld5.py
+++ oppg1B_bld5.py
@@ -1,2 +1,7 @@
 tall = 5
 print(tall)
+
+celcuis = input("Skriv inn en temperatur i celcuis: ")
+celcuis = int(celcuis)
+Farenheit = (celcuis * 9/5) + 32
+print("Dette er temperaturen din i farenheit ", Farenheit)
