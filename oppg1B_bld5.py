94be89097922cbafaba01f25c6499da4918ae1e0
gullhenrik <henrikg@sf-nett.no>
2023-09-19 12:44:46 +0200

    Update oppg1B_bld5.py
    legger til F til C kalkulator

--- oppg1B_bld5.py
+++ oppg1B_bld5.py
@@ -1,2 +1,14 @@
 tall = 5
 print(tall)
+
+
+
+
+
+
+
+
+fahrenheit = input("Skriv inn en tempteratur i Fahrenheit")
+fahrenheit = int(fahrenheit)
+celsius = (fahrenheit-32)*5/9
+print(f"{fahrenheit} grader Fahrenheit er det samme som {celsius} grader celsius.")
