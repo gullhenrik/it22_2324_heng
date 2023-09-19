279426da0e58169ce816f7ffec185280fe4f6f51
HussamBasim <basimhussam1@gmail.com>
2023-09-19 12:45:39 +0200

    pseudokode

--- oppg1B_bld5.py
+++ oppg1B_bld5.py
@@ -1,7 +1,14 @@
 tall = 5
 print(tall)
 
+#lag en input for bruker med en sammenhengende tekst
+#gjør strengen fra en sting til integer 
+#gi koden formelen for celcius til farenheit 
+#gjør så koden viser svaret med en sammenhengede tekst
+
+
 celcuis = input("Skriv inn en temperatur i celcuis: ")
 celcuis = int(celcuis)
 Farenheit = (celcuis * 9/5) + 32
 print("Dette er temperaturen din i farenheit ", Farenheit)
+
